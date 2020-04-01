25eb72dd8b1efe2b9ac39c73505fdb18c7e132b0
Marco Nicola <marconicola@disroot.org>
2020-04-01 22:22:57 +0200

    Create main package draft

--- goslide.go
+++ goslide.go
@@ -2,4 +2,38 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package goslide
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/nlpodyssey/goslide/configuration"
+)
+
+var logger = log.New(os.Stderr, "", 0)
+
+func main() {
+	validateArguments()
+	loadGlobalConfiguration()
+
+}
+
+func validateArguments() {
+	if len(os.Args) != 2 {
+		logger.Println("Invalid or malformed arguments.")
+		logger.Fatal("\nUsage:\n  goslide <json_configuration_file>\n")
+	}
+}
+
+func loadGlobalConfiguration() {
+	configFilename := os.Args[1]
+	config, err := configuration.FromJsonFile(configFilename)
+
+	if err != nil {
+		logger.Println("An error occurred reading the configuration file.")
+		logger.Fatal(err)
+	}
+
+	configuration.Global = config
+}
