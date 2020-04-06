9053dfbcd490c9a2e674a59e4c88d0a058a3f6f9
Marco Nicola <marconicola@disroot.org>
2020-04-06 08:17:50 +0200

    Create LSH

---
+++ lsh/lsh.go
@@ -0,0 +1,145 @@
+// Copyright (c) 2020, The GoSLIDE Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package lsh
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+
+	"github.com/nlpodyssey/goslide/bucket"
+	"github.com/nlpodyssey/goslide/configuration"
+)
+
+const binSize = 8
+
+var logBinSize = int(math.Floor(math.Log(float64(binSize))))
+
+type LSH struct {
+	buckets  [][]*bucket.Bucket
+	k        int
+	l        int
+	rangePow int
+	rand1    []int
+}
+
+func New(k, l, rangePow int) *LSH {
+	buckets := make([][]*bucket.Bucket, l)
+
+	// TODO: parallel?
+	for i := range buckets {
+		buckets[i] = make([]*bucket.Bucket, 1<<rangePow)
+		for j := range buckets[i] {
+			buckets[i][j] = bucket.New()
+		}
+	}
+
+	rand1 := make([]int, k*l)
+
+	// TODO: parallel?
+	for i := range rand1 {
+		rand1[i] = positiveOddRandomInt()
+	}
+
+	return &LSH{
+		buckets:  buckets,
+		k:        k,
+		l:        l,
+		rangePow: rangePow,
+		rand1:    rand1,
+	}
+}
+
+func (lsh *LSH) Clear() {
+	for i := range lsh.buckets {
+		lsh.buckets[i] = make([]*bucket.Bucket, 1<<lsh.rangePow)
+		for j := range lsh.buckets[i] {
+			lsh.buckets[i][j] = bucket.New()
+		}
+	}
+}
+
+func (lsh *LSH) Count() {
+	for i, bi := range lsh.buckets {
+		total := 0
+		for _, bj := range bi {
+			size := bj.GetSize()
+			if size != 0 {
+				fmt.Printf("%d ", size)
+			}
+			total += size
+		}
+		fmt.Printf("\nTABLE %d Total %d\n", i, total)
+	}
+}
+
+func (lsh *LSH) HashesToIndex(hashes []int) []int {
+	hashFunction := configuration.Global.HashFunction
+
+	indices := make([]int, lsh.l)
+
+	for i := range indices {
+		index := 0
+
+		for j := 0; j < lsh.k; j++ {
+			switch hashFunction {
+			case configuration.WtaHashFunction, configuration.DensifiedWtaHashFunction:
+				h := hashes[lsh.k*i+j]
+				index += h << ((lsh.k - 1 - j) * logBinSize)
+			case configuration.DensifiedMinhashFunction:
+				randVal := lsh.rand1[lsh.k*i+j]
+				h := randVal
+				h *= randVal
+				h ^= h >> 13
+				h ^= lsh.rand1[lsh.k*i+j]
+				index += h * hashes[lsh.k*i+j]
+			case configuration.SparseRandomProjectionHashFunction:
+				h := hashes[lsh.k*i+j]
+				index += h << (lsh.k - 1 - j)
+			}
+		}
+
+		if hashFunction == configuration.DensifiedMinhashFunction {
+			index = index & ((1 << lsh.rangePow) - 1)
+		}
+
+		indices[i] = index
+	}
+
+	return indices
+}
+
+func (lsh *LSH) Add(indices []int, id int) []int {
+	secondIndices := make([]int, lsh.l)
+	for i := range secondIndices {
+		secondIndices[i] = lsh.buckets[i][indices[i]].Add(id)
+	}
+	return secondIndices
+}
+
+func (lsh *LSH) AddSingle(tableId, index, id int) int {
+	return lsh.buckets[tableId][index].Add(id)
+}
+
+// Returns all the buckets
+func (lsh *LSH) RetrieveRaw(indices []int) [][]int {
+	rawResults := make([][]int, lsh.l)
+	for i := range rawResults {
+		rawResults[i] = lsh.buckets[i][indices[i]].GetAll()
+	}
+	return rawResults
+}
+
+func (lsh *LSH) Retrieve(table, index, bucket int) int {
+	return lsh.buckets[table][index].Retrieve(bucket)
+}
+
+func positiveOddRandomInt() int {
+	n := rand.Int()
+	if n%2 == 0 {
+		return n + 1
+	}
+	return n
+}
---
+++ lsh/lsh_test.go
@@ -0,0 +1,132 @@
+// Copyright (c) 2020, The GoSLIDE Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package lsh
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLSHNew(t *testing.T) {
+	lsh := New(3, 4, 10)
+
+	assertIntEqual(t, lsh.k, 3, "k")
+	assertIntEqual(t, lsh.l, 4, "l")
+	assertIntEqual(t, lsh.rangePow, 10, "rangePow")
+	assertIntEqual(t, len(lsh.buckets), 4, "len(buckets)")
+	assertIntEqual(t, len(lsh.rand1), 12, "len(rand1)")
+
+	for _, b := range lsh.buckets {
+		assertIntEqual(t, len(b), 0b10000000000, "len(buckets[])")
+	}
+
+	for i, r := range lsh.rand1 {
+		isPositiveOdd(t, r, fmt.Sprintf("rand1[%d]", i))
+	}
+}
+
+func TestLSHAdd(t *testing.T) {
+	lsh := New(3, 4, 10)
+
+	result := lsh.Add([]int{1, 10, 100, 1000}, 4321)
+	assertIntSliceEqual(t, result, []int{0, 0, 0, 0}, "Add first result")
+
+	result = lsh.Add([]int{2, 10, 200, 1000}, 4321)
+	assertIntSliceEqual(t, result, []int{0, 1, 0, 1}, "Add second result")
+}
+
+func TestLSHAddSingle(t *testing.T) {
+	lsh := New(3, 4, 10)
+
+	result := lsh.AddSingle(0, 0, 123)
+	assertIntEqual(t, result, 0, "Add first result")
+
+	result = lsh.AddSingle(1, 1, 456)
+	assertIntEqual(t, result, 0, "Add second result")
+
+	result = lsh.AddSingle(0, 0, 789)
+	assertIntEqual(t, result, 1, "Add third result")
+}
+
+func TestLSHRetrieveRaw(t *testing.T) {
+	lsh := New(3, 4, 10)
+	lsh.Add([]int{1, 10, 100, 1000}, 4321)
+	result := lsh.RetrieveRaw([]int{1, 10, 100, 1000})
+
+	assertIntEqual(t, len(result), 4, "len(RetrieveRaw)")
+
+	for i, r := range result {
+		assertIntEqual(t, len(r), 128, fmt.Sprintf("len(RetrieveRaw[%d])", i))
+
+		assertIntEqual(t, r[0], 4321, fmt.Sprintf("RetrieveRaw[%d][0]", i))
+		assertIntEqual(t, r[1], -1, fmt.Sprintf("RetrieveRaw[%d][1]", i))
+	}
+}
+
+func TestLSHRetrieve(t *testing.T) {
+	lsh := New(3, 4, 10)
+	lsh.AddSingle(2, 3, 123)
+
+	result := lsh.Retrieve(2, 3, 0)
+	assertIntEqual(t, result, 123, "Retrieve")
+}
+
+func TestLSHHashesToIndex(t *testing.T) {
+	// Just ensure no error is raised
+	lsh := New(3, 4, 10)
+	result := lsh.HashesToIndex([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12})
+	assertIntEqual(t, len(result), 4, "len(HashesToIndex)")
+}
+
+func TestLSHClear(t *testing.T) {
+	lsh := New(3, 4, 10)
+	lsh.AddSingle(2, 3, 123)
+
+	result := lsh.Retrieve(2, 3, 0)
+	assertIntEqual(t, result, 123, "Retrieve before Clear")
+
+	lsh.Clear()
+
+	result = lsh.Retrieve(2, 3, 0)
+	assertIntEqual(t, result, 0, "Retrieve after Clear")
+}
+
+func TestLSHCount(t *testing.T) {
+	// Just ensure no error is raised
+	lsh := New(3, 4, 10)
+	lsh.Add([]int{1, 10, 100, 1000}, 4321)
+	lsh.Add([]int{2, 10, 200, 1000}, 9876)
+	lsh.Count()
+}
+
+func isPositiveOdd(t *testing.T, n int, msg string) {
+	if n == 0 {
+		t.Errorf("Assertion failed: %s | expected %d to be non zero", msg, n)
+	} else if n%2 == 0 {
+		t.Errorf("Assertion failed: %s | expected %d to odd", msg, n)
+	}
+}
+
+func assertIntEqual(t *testing.T, actual, expected int, msg string) {
+	if actual != expected {
+		t.Errorf("Assertion failed: %s | expected %d, actual %d",
+			msg, expected, actual)
+	}
+}
+
+func assertIntSliceEqual(t *testing.T, actual, expected []int, msg string) {
+	if len(actual) != len(expected) {
+		t.Errorf("Lengths differ: %s | expected %d for %v, actual %d for %v",
+			msg, len(expected), expected, len(actual), actual)
+		return
+	}
+
+	for i, expVal := range expected {
+		if actVal := actual[i]; expVal != actVal {
+			t.Errorf("%s | values at %d differ: expected %d, actual %d",
+				msg, i, expVal, actVal)
+		}
+	}
+}
